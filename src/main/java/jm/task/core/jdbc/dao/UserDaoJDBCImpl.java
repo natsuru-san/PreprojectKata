094220a0c1912cd2d522a81f38566e9a1c26d246
natsuru <natsuru@null.net>
2022-06-11 12:39:06 +0300

    Виправлення до задачі

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,24 +14,47 @@ public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {}
 
     public void createUsersTable() {
-        exec("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age SMALLINT);");
+        applyCommandOnDatabase("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age SMALLINT);");
     }
 
     public void dropUsersTable() {
-        exec("DROP TABLE IF EXISTS users;");
+        applyCommandOnDatabase("DROP TABLE IF EXISTS users;");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        exec("INSERT INTO users (name, lastName, age) VALUES ('" + name + "', '" + lastName + "', '" + age + "');");
+        try (Connection connection = new Util().getConnection()) {
+            String cmd = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?);";
+            PreparedStatement statement = connection.prepareStatement(cmd);
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-        exec("DELETE FROM users WHERE id='" + id + "';");
+        try (Connection connection = new Util().getConnection()) {
+            String cmd = "DELETE FROM users WHERE id=?;";
+            PreparedStatement statement = connection.prepareStatement(cmd);
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        try {
-            return new Util().getUsers("SELECT * FROM users;");
+        try (Connection connection = new Util().getConnection()) {
+            List<User> list = new ArrayList<>();
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users;");
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                list.add(user);
+            }
+            return list;
         } catch (SQLException e) {
             LOG.log(Level.SEVERE, "Помилка роботи з БД (GET), дивись информацию тут: " + e);
         }
@@ -39,11 +62,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        exec("DELETE FROM users;");
+        applyCommandOnDatabase("DELETE FROM users;");
     }
-    private void exec(String cmd) {
-        try {
-            new Util().execCmd(cmd);
+    private void applyCommandOnDatabase(String cmd) {
+        try (Connection connection = new Util().getConnection()) {
+            connection.createStatement().execute(cmd);
         } catch (SQLException e) {
             LOG.log(Level.SEVERE, "Помилка роботи з БД, дивись информацию дале: " + e);
         }
